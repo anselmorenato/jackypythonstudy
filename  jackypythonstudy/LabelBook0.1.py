188efc5fe78d2984793512b9130d75000fb3926e
jackyma1981 <jackyma1981@6c34ee4c-5f35-11de-b59c-372aa07dcf7f>
2009-06-24 12:42:25 +0000

    successfully file

---  jackypythonstudy/LabelBook0.1.py
+++  jackypythonstudy/LabelBook0.1.py
@@ -1,7 +1,5 @@
 import wx
 import wx.lib.colourselect as csel
-#import random
-
 import os
 import sys
 
@@ -11,7 +9,7 @@ try:
 except:
     dirName = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-bitmapDir = os.path.join(dirName, 'bitmaps')
+bitmapDir = os.path.join(dirName, 'images')
 sys.path.append(os.path.split(dirName)[0])
 
 try:
@@ -20,101 +18,83 @@ try:
 except ImportError: # if it's not there locally, try the wxPython lib.
     import wx.lib.agw.labelbook as LB
     from wx.lib.agw.fmresources import *
-
-
-
+   
+#import images
 _pageTexts = ["Hello", "From", "wxPython", "LabelBook", "Demo"]
-_pageIcons = []#["roll.png", "charge.png", "add.png", "decrypted.png", "news.png"]
+#_pageIcons = []
+_pageIcons = ["roll.png", "charge.png", "add.png", "decrypted.png", "news.png"]
+
+#_pageIcons = ["icon-equiv.png","icon-minimiz.png","icon-product.png","icon-setup.png","product4x.png"]
 _pageColours = [wx.RED, wx.GREEN, wx.WHITE, wx.BLUE, "Pink"]
 
-
-class LabelBookDemo(wx.Frame):
-
-    def __init__(self, parent, log):
-
+class LabelBook(wx.Frame):
+    def __init__(self, parent):
         wx.Frame.__init__(self, parent)
-
-        self.initializing = True
-
-        self.log = log
-       
         self.mainpanel = wx.Panel(self, -1)
-       
+      
         self.SetProperties()
         self.CreateLabelBook()
         self.DoLayout()
 
-        
-        self.SetSize((800,700))
+        self.SetSize((600,500))
 
-      
+        #self.SetIcon(images.Mondrian.GetIcon()) 
         self.CenterOnScreen()
 
-        self.initializing = False
+        #self.initializing = False
         self.SendSizeEvent()
-
-
+       
     def SetProperties(self):
 
-        self.SetTitle("This is my code souce!-)")
-        #sself.pin.SetValue(1)
-        #self.mainpanel.SetMinimumPaneSize(120)
-
-
+        self.SetTitle("The success labelbook!-)")
+        
+       
     def DoLayout(self):
-
         mainsizer = wx.BoxSizer(wx.VERTICAL)
         panelsizer = wx.BoxSizer(wx.VERTICAL)
-        
-
+       
         panelsizer.Add(self.book, 1, wx.EXPAND, 0)
         self.mainpanel.SetSizer(panelsizer)
         panelsizer.Layout()
-
-        
+      
         mainsizer.Add(self.mainpanel, 1, wx.EXPAND, 0)
 
         self.SetSizer(mainsizer)
         mainsizer.Layout()
         self.Layout()
 
-
     def CreateLabelBook(self):
-
-
-        style = self.GetBookStyles()
-
-        self.book = LB.LabelBook(self.mainpanel, -1, style=style)
-           
-        #self.book = LB.FlatImageBook(self.mainpanel, -1, style=style)
-
-        #self.EnableChoices(btype)            
+        style = self.GetBookStyles() 
+               
+        self.book = LB.LabelBook(self.mainpanel, -1,style=style)                     
+        #self.book = LB.FlatImageBook(self.mainpanel, -1, style=style)           
 
         self.imagelist = self.CreateImageList()
         self.book.AssignImageList(self.imagelist)
+        self.Refresh()
 
         for indx, txts in enumerate(_pageTexts):
-           
+            #label = "This is panel number %d"%(indx+1)
             self.book.AddPage(TestPanel(self.book,_pageColours[indx]),
                               txts, True, indx)
 
-        self.book.SetSelection(0)            
-
+        self.book.SetSelection(0)
+        #self.book.SetWindowStyleFlag(style=LB.INB_FIT_BUTTON)
         
         self.SendSizeEvent()
- 
+
     def GetBookStyles(self):
-
         style = INB_FIT_BUTTON
-        #style = self.GetBookOrientation(style)
-
-        #style = INB_WEB_HILITE
-        #style = INB_USE_PIN_BUTTON
         
-        #style = INB_BORDER
-        #style = INB_SHOW_ONLY_TEXT
+        style |= LB.INB_FIT_LABELTEXT
+        #style |= INB_WEB_HILITE
+        #style |= INB_USE_PIN_BUTTON
+        #style |= INB_BORDER
+        #style |= LB.INB_SHOW_ONLY_TEXT   #only used in FlatImageBook type
         
-        return style            
+        style = self.GetBookOrientation(style)
+        
+        return style           
 
 
     def CreateImageList(self):
@@ -124,51 +104,28 @@ class LabelBookDemo(wx.Frame):
             newImg = os.path.join(bitmapDir, "lb%s"%img)
             bmp = wx.Bitmap(newImg, wx.BITMAP_TYPE_PNG)
             imagelist.Add(bmp)
-      
-        return imagelist
+       
+        #bmp = imagebmp.GetBitmap()
+        #for i in range(5):
+        #    imagelist.Add(bmp)
 
+        return imagelist
 
     def GetBookOrientation(self, style):
 
-        style |= INB_LEFT #selection = self.bookdirection.GetSelection()
-        #if selection == 0:
-            
-        #elif selection == 1:
+        style |= INB_LEFT
         #style |= INB_RIGHT
-        #elif selection == 2:
-        #    style |= INB_TOP
-        #else:
-        #    style |= INB_BOTTOM
+        #style |= LB.INB_TOP   #only used in FlatImageBook type
+        #style |= INB_BOTTOM  #only used in FlatImageBook type
 
         return style
-
-
-    def OnBookType(self, event):
-
-        self.CreateLabelBook(event.GetInt())
-        event.Skip()
-
-
-    def OnBookOrientation(self, event):
-
-        style = self.GetBookStyles()
-        self.book.SetWindowStyleFlag(style)
-
-        event.Skip()
-
-
-    
-    
-
-
+  
 #---------------------------------------------------------------------------
-
-
 class TestPanel(wx.Panel):
     def __init__(self, parent,colour):
         #self.log = log
         wx.Panel.__init__(self, parent, -1)
-        
+       
         st = wx.StaticText(self, -1,
                           "You can put nearly any type of window here,\n"
                           "and if the platform supports it then the\n"
@@ -176,8 +133,8 @@ class TestPanel(wx.Panel):
                           (10, 10))
 
         b = wx.Button(self, -1, " Test LabelBook And FlatImageBook ", (100,100))
-        
-        
+       
+       
         self.Bind(wx.EVT_BUTTON, self.OnButton, b)
         self.SetBackgroundColour(colour)
 
@@ -186,14 +143,11 @@ class TestPanel(wx.Panel):
         dlg = wx.MessageDialog(self,msg,"this my test",wx.OK | wx.ICON_INFORMATION)
         dlg.ShowModal()
         dlg.Destroy()
-        
-
-
-
+ 
 overview = LB.__doc__
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
-    frame = LabelBookDemo(None,-1)
+    frame = LabelBook(None)
     frame.Show()
     app.MainLoop()
