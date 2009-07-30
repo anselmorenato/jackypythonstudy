79eeb6e269796ec1d2b207fd9c163ffecd8ea55b
jackyma1981 <jackyma1981@6c34ee4c-5f35-11de-b59c-372aa07dcf7f>
2009-07-30 06:36:46 +0000

    点击列表项目，treelist 的root 能够改变

---  jackypythonstudy/remotepanel.py
+++  jackypythonstudy/remotepanel.py
@@ -41,6 +41,71 @@ ArtIDs = [ "None",
            "wx.ART_MISSING_IMAGE",
            "SmileBitmap"
            ]
+remote_configs = dict(
+    email = 'ishikura@gifu-u.ac.jp',
+    local = dict(
+        ssh = {},
+        rootdir = 'C:\path\to\nagara-root',
+        workdir = '',
+        jms = dict(
+            Single = dict(
+                envs = {},
+                path = {},
+            ),
+            MultiProcess = dict(
+            ),
+        ),
+        commands = {},
+    ),
+    hpcs = dict(
+        ssh = dict(
+            address = '133.66.117.139',
+            user = 'ishikura',
+            passwd = '*********',
+            port = 22,
+        ),
+        rootdir = '/home/ishikura/Nagara/projects',
+        workdir = '/work/ishikura',
+        # jms = ['Single', 'MPI', 'LSF'], #Local
+        jms = dict(
+            Single = dict(
+                envs = {},
+                path = {},
+            ),
+            MPI = dict(
+                envs = {},
+                path = {},
+            ),
+            LSF = dict(
+                envs = {},
+                path = {},
+                script = {},
+            ),
+        ),
+        commands = dict(
+            amber = dict(
+                # envs = dict(AMBERHOME = '/home/hpc/opt/amber10.eth'),
+                # path = '/home/hpcs/opt/amber10.eth/exe/sander.MPI',
+                envs = dict(AMBERHOME = '/home/ishikura/opt/amber10.eth'),
+                path = '/home/ishikura/opt/amber10.eth/exe/sander.MPI',
+            ),
+            marvin = dict(
+                envs = {},
+                # path = '/home/hpcs/Nagara/app/bin/marvin',
+                path = '/home/ishikura/Nagara/app/bin/marvin',
+            ),
+            paics = dict(
+                # envs = dict(PAICS_HOME='/home/ishi/paics/paics-20081214'),
+                # path = '/home/ishi/paics/paics-20081214/main.exe',
+                envs = dict(PAICS_HOME='/home/ishi/paics/paics-20081214'),
+                path = '/home/ishi/paics/paics-20081214/main.exe',
+            ),
+        ),
+    ),
+    vlsn = dict(),
+    rccs = dict(),
+)
+#item = None
 class RemotePanel(wx.Panel):
     def __init__(self, parent,id,size):
         wx.Panel.__init__(self,parent,-1,size=(500,500))
@@ -49,14 +114,17 @@ class RemotePanel(wx.Panel):
         #self.splitter = wx.SplitterWindow(self, ID_SPLITTER, style=wx.SP_BORDER)
         #panel = wx.Panel(splitter, -1, style=wx.WANTS_CHARS)
         choice =['vlsn','rccs','lacal','email','phcs']
+       
         listbox = wx.ListBox(self, size = (130,50),
                        choices=choice)
-        tag = listbox.GetSelection()
+        #tag = listbox.GetSelection()
         listbox.SetFont(wx.Font(11,wx.SWISS,wx.NORMAL,wx.NORMAL))
         #listbox.SetSize(listbox.GetBestFittingSize())
         listbox.Bind(wx.EVT_LISTBOX_DCLICK, self.EvtListBoxDClick, listbox)
+        listbox.Bind(wx.EVT_LISTBOX, self.EvtListBox, listbox)
         #htl = HyperTreeListPanel(self)
-        tree = HyperTreeList(self,-1,size=(500,600),style=wx.TR_DEFAULT_STYLE)
+        self.tree = HyperTreeList(self,-1,size=(500,600),style=wx.TR_DEFAULT_STYLE)
+        self.root = self.tree.root
         
         okBtn = wx.Button(self, -1, "Ok")
         cancelBtn = wx.Button(self, -1, "Cancel")
@@ -103,15 +171,17 @@ class RemotePanel(wx.Panel):
         #mainsizer.Add((5,20),wx.EXPAND,5)
         mainsizer.Add(sizer_1,0,wx.ALIGN_CENTER, 5)
         #mainsizer.Add((5,20),wx.EXPAND,5)
-        mainsizer.Add(tree,1,wx.EXPAND,5)
+        mainsizer.Add(self.tree,0,wx.EXPAND,5)
         mainsizer.Add(sizer_2,0,wx.ALIGN_RIGHT, 5)
         
         
-        self.SetSizer(mainsizer)
-        mainsizer.Fit(self)
-        mainsizer.SetSizeHints(self)
-        mainsizer.Layout()
         
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(mainsizer,0,wx.EXPAND)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        #sizer.SetSizeHints(self)
+        sizer.Layout()
     def OnClick(self, event): pass
     def OnCancel(self,event):
         self.Close()
@@ -126,6 +196,20 @@ class RemotePanel(wx.Panel):
             dlg = settingdlg.SettingDialog(None,-1)
             dlg.Show()
             event.Skip()
+    def EvtListBox(self,event):
+        '''
+        '''
+        #lb = event.GetEventObject()
+        
+        
+        self.root = self.tree.SetItemText(self.tree.GetRootItem(),event.GetString()) 
+        #self.tree.SetItemText(self.tree.GetItemText(1),event.GetString())
+        #item = self.tree.SetItemText(self.tree.GetFirstChild(),event.GetString())#event.GetString()
+        #self.text = parent.GetParent().rightPanel.text
+        #item = event.GetString()
+
+        
+        
         
     '''        
 class HyperTreeListPanel(wx.Panel):
@@ -156,91 +240,7 @@ class HyperTreeList(HTL.HyperTreeList):
 
         HTL.HyperTreeList.__init__(self, parent, id, pos, size, style)
         
-        alldata = dir(HTL)
-        
-        remote_configs = dict(
-            email = 'ishikura@gifu-u.ac.jp',
-            local = dict(
-                ssh = {},
-                rootdir = 'C:\path\to\nagara-root',
-                workdir = '',
-                jms = dict(
-                    Single = dict(
-                        envs = {},
-                        path = {},
-                    ),
-                    MultiProcess = dict(
-                    ),
-                ),
-                commands = {},
-            ),
-            hpcs = dict(
-                ssh = dict(
-                    address = '133.66.117.139',
-                    user = 'ishikura',
-                    passwd = '*********',
-                    port = 22,
-                ),
-                rootdir = '/home/ishikura/Nagara/projects',
-                workdir = '/work/ishikura',
-                # jms = ['Single', 'MPI', 'LSF'], #Local
-                jms = dict(
-                    Single = dict(
-                        envs = {},
-                        path = {},
-                    ),
-                    MPI = dict(
-                        envs = {},
-                        path = {},
-                    ),
-                    LSF = dict(
-                        envs = {},
-                        path = {},
-                        script = {},
-                    ),
-                ),
-                commands = dict(
-                    amber = dict(
-                        # envs = dict(AMBERHOME = '/home/hpc/opt/amber10.eth'),
-                        # path = '/home/hpcs/opt/amber10.eth/exe/sander.MPI',
-                        envs = dict(AMBERHOME = '/home/ishikura/opt/amber10.eth'),
-                        path = '/home/ishikura/opt/amber10.eth/exe/sander.MPI',
-                    ),
-                    marvin = dict(
-                        envs = {},
-                        # path = '/home/hpcs/Nagara/app/bin/marvin',
-                        path = '/home/ishikura/Nagara/app/bin/marvin',
-                    ),
-                    paics = dict(
-                        # envs = dict(PAICS_HOME='/home/ishi/paics/paics-20081214'),
-                        # path = '/home/ishi/paics/paics-20081214/main.exe',
-                        envs = dict(PAICS_HOME='/home/ishi/paics/paics-20081214'),
-                        path = '/home/ishi/paics/paics-20081214/main.exe',
-                    ),
-                ),
-            ),
-            vlsn = dict(),
-            rccs = dict(),
-        )
-
-        treestyles = []
-        events = []
-        for data in alldata:
-            if data.startswith("TR_"):
-                treestyles.append(data)
-            elif data.startswith("EVT_"):
-                events.append(data)
-                
-        events = events + [i for i in dir(wx) if i.startswith("EVT_TREE_")]
-        for evt in ["EVT_TREE_GET_INFO", "EVT_TREE_SET_INFO", "EVT_TREE_ITEM_MIDDLE_CLICK",
-                    "EVT_TREE_STATE_IMAGE_CLICK"]:
-            events.remove(evt)
-            
-        treestyles = treestyles + [i for i in dir(wx) if i.startswith("TR_")]
-
-        self.events = events
-        self.styles = treestyles
-        self.item = None
+       
         
         il = wx.ImageList(16, 16)
 
@@ -265,56 +265,58 @@ class HyperTreeList(HTL.HyperTreeList):
         self.AddColumn("Main column")
         self.AddColumn("Column 1")
         self.AddColumn("Column 2")
-        self.SetMainColumn(1) # the one with the tree in it...
+        self.SetMainColumn(0) # the one with the tree in it...
         self.SetColumnWidth(0, 175)
 
-        self.root = self.AddRoot("The Root Item")
+        self.root = self.AddRoot('root')
 
-        if not(self.GetWindowStyle() & wx.TR_HIDE_ROOT):
-            self.SetPyData(self.root, None)
-            self.SetItemImage(self.root, 24, which=wx.TreeItemIcon_Normal)
-            #self.SetItemImage(self.root, 13, which=wx.TreeItemIcon_Expanded)
-            self.SetItemText(self.root, "col 1 root", 1)
-            self.SetItemText(self.root, "col 2 root", 2)
-             
-            for item, val in remote_configs.items():
-                #txt = "Item %d" % x
-                
-                child = self.AppendItem(self.root, item)
-    
-                self.SetPyData(child, None)
-                self.SetItemText(child, item, 1)
-                self.SetItemText(child, item + " (c2)", 2)
-                self.SetItemImage(child, 24, which=wx.TreeItemIcon_Normal)
-                self.SetItemImage(child, 13, which=wx.TreeItemIcon_Expanded)
-                
+        
+        self.SetPyData(self.root, None)
+        self.SetItemImage(self.root, 24, which=wx.TreeItemIcon_Normal)
+        self.SetItemImage(self.root, 13, which=wx.TreeItemIcon_Expanded)
+        self.SetItemText(self.root, "col 1 root", 1)
+        self.SetItemText(self.root, "col 2 root", 2)
+        
+        item = self.GetRootItem()
+        print item
+        for item, val in remote_configs.items():
+            #txt = "Item %d" % x
             
-                if isinstance(val,dict):
-                    for item2,val in remote_configs[item].items():
+            child = self.AppendItem(self.root, item)
+
+            self.SetPyData(child, None)
+            self.SetItemText(child, item, 1)
+            self.SetItemText(child, item + " (c2)", 2)
+            self.SetItemImage(child, 24, which=wx.TreeItemIcon_Normal)
+            self.SetItemImage(child, 13, which=wx.TreeItemIcon_Expanded)
+            
+        
+            if isinstance(val,dict):
+                for item2,val in remote_configs[item].items():
+                    
+                    
+                    child2 = self.AppendItem(child, item2)
+                    self.SetPyData(child2, None)
+                    self.SetItemText(child2, item2[:-1], 1)
+                    self.SetItemImage(child2, 24, which=wx.TreeItemIcon_Normal)
+                    self.SetItemImage(child2, 13, which=wx.TreeItemIcon_Expanded)
+                    if isinstance(val,dict):
                         
+                        for item3,val in remote_configs[item][item2].items():
+                            
+                            child3 = self.AppendItem(child2,item3)
+                            
+                            self.SetPyData(child3,None)
+                            self.SetItemText(child3, item3[:-1], 1)
+                            self.SetItemImage(child3, 24, which=wx.TreeItemIcon_Normal)
+                            self.SetItemImage(child3, 13, which=wx.TreeItemIcon_Expanded)
+                            if isinstance(val,dict):
+                                for item4,val in remote_configs[item][item2][item3].items():
+                                    child4 = self.AppendItem(child3,item4)
+                                    self.SetPyData(child4,None)
+                                    self.SetItemText(child4, item4[:-1], 1)
+                                
                         
-                        child2 = self.AppendItem(child, item2)
-                        self.SetPyData(child2, None)
-                        self.SetItemText(child2, item2[:-1], 1)
-                        self.SetItemImage(child2, 24, which=wx.TreeItemIcon_Normal)
-                        self.SetItemImage(child2, 13, which=wx.TreeItemIcon_Expanded)
-                        if isinstance(val,dict):
-                            
-                            for item3,val in remote_configs[item][item2].items():
-                                
-                                child3 = self.AppendItem(child2,item3)
-                                
-                                self.SetPyData(child3,None)
-                                self.SetItemText(child3, item3[:-1], 1)
-                                self.SetItemImage(child3, 24, which=wx.TreeItemIcon_Normal)
-                                self.SetItemImage(child3, 13, which=wx.TreeItemIcon_Expanded)
-                                if isinstance(val,dict):
-                                    for item4,val in remote_configs[item][item2][item3].items():
-                                        child4 = self.AppendItem(child3,item4)
-                                        self.SetPyData(child4,None)
-                                        self.SetItemText(child4, item4[:-1], 1)
-                                    
-                            
 def main():
     import nagaratest
     app = nagaratest.FrameTest()
